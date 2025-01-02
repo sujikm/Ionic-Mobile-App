49e78647446ae24f3cbfd1d44af65de9d403ffbe
sujikm <kmsujinair@gmail.com>
2025-01-02 13:59:53 +0000

    Updated to Ion-input  from label and used ngModel to store the value in a variable

--- G00439359/src/app/home/home.page.ts
+++ G00439359/src/app/home/home.page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton} from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton,IonInput} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonButton,RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonButton,RouterLink,IonInput,FormsModule],
 })
 export class HomePage {
+  united:string=""
   constructor() {}
 }
