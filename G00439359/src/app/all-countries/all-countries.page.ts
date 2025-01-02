a583875fb5a109e86fa938f2a388f91741a788dd
sujikm <kmsujinair@gmail.com>
2025-01-02 17:52:03 +0000

    I put the url of the data from the internet as options in HttpOptions and concatenated the value from the input and got the data from the url and printed int he console

--- G00439359/src/app/all-countries/all-countries.page.ts
+++ G00439359/src/app/all-countries/all-countries.page.ts
@@ -1,25 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardSubtitle, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
 import { DataService } from '../services/data.service';
+import { MyHttpService } from '../services/my-http.service';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-all-countries',
   templateUrl: './all-countries.page.html',
   styleUrls: ['./all-countries.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardTitle, IonCardHeader, IonCardSubtitle, IonCardContent, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class AllCountriesPage implements OnInit {
 united:string= "";
-  constructor(private ds:DataService) { }
+countryinfo!:any;
+options:HttpOptions={
+  url:"https://restcountries.com/v3.1/name/"
+}
+  constructor(private ds:DataService,private mhs:MyHttpService) { }
 
   ngOnInit() {
     this.getUN();
   }
   async getUN(){
     this.united=await this.ds.get('UN');
+    this.options.url=this.options.url.concat(this.united)
+   this.countryinfo=await this.mhs.get(this.options)
+   console.log(JSON.stringify(this.countryinfo.data))
   }
 
 }
