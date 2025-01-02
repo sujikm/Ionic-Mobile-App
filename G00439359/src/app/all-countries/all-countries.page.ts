0795c3597f19132ec5fd731a3c6debaa76b2b300
sujikm <kmsujinair@gmail.com>
2025-01-02 20:30:41 +0000

    used an ion-card and for each countryinfo displayed the flag,official name and two buttons

--- G00439359/src/app/all-countries/all-countries.page.ts
+++ G00439359/src/app/all-countries/all-countries.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardSubtitle, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardSubtitle, IonCardHeader, IonCardTitle, IonButton } from '@ionic/angular/standalone';
 import { DataService } from '../services/data.service';
 import { MyHttpService } from '../services/my-http.service';
 import { HttpOptions } from '@capacitor/core';
@@ -11,7 +11,7 @@ import { HttpOptions } from '@capacitor/core';
   templateUrl: './all-countries.page.html',
   styleUrls: ['./all-countries.page.scss'],
   standalone: true,
-  imports: [IonCardTitle, IonCardHeader, IonCardSubtitle, IonCardContent, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonCardTitle, IonCardHeader,  IonCardContent, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class AllCountriesPage implements OnInit {
 united:string= "";
@@ -27,8 +27,9 @@ options:HttpOptions={
   async getUN(){
     this.united=await this.ds.get('UN');
     this.options.url=this.options.url.concat(this.united)
-   this.countryinfo=await this.mhs.get(this.options)
-   console.log(JSON.stringify(this.countryinfo.data))
+   let result=await this.mhs.get(this.options)
+   this.countryinfo=result.data
+   console.log(JSON.stringify(this.countryinfo))
   }
 
 }
