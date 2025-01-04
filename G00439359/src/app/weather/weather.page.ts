8d02f9c34d5729e83a2f5143e34545086ec7c8c0
sujikm <kmsujinair@gmail.com>
2025-01-04 01:03:24 +0000

    Added weather card component to display the weather description, temperature, and unit dynamically.

--- G00439359/src/app/weather/weather.page.ts
+++ G00439359/src/app/weather/weather.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 import { DataService } from '../services/data.service';
 import { HttpOptions } from '@capacitor/core';
 import { MyHttpService } from '../services/my-http.service';
@@ -11,13 +11,16 @@ import { MyHttpService } from '../services/my-http.service';
   templateUrl: './weather.page.html',
   styleUrls: ['./weather.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class WeatherPage implements OnInit {
 selectedUnit: string=""
 Latitude:string=""
 Longitude:string=""
 weatherinfo!:any;
+capital:string=""
+desc:string=""
+temp:number=0;
 options:HttpOptions={
   url:""
 }
@@ -29,6 +32,7 @@ options:HttpOptions={
   async getWeatherFromStorage(){
     this.selectedUnit = await this.ds.get('unit') || 'Metric'; // Default to 'Metric' if nothing is saved
     console.log('Selected Unit:', this.selectedUnit);
+    this.capital=await this.ds.get("capital");
     this.Latitude=await this.ds.get('latitude')
     this.Longitude=await this.ds.get('longitude')
     console.log(this.Latitude)
@@ -37,6 +41,10 @@ options:HttpOptions={
     console.log(this.options.url)
     let result=await this.mhs.get(this.options)
     this.weatherinfo=result.data
+    this.desc=this.weatherinfo.weather[0].description 
+    this.temp = this.weatherinfo.main.temp;
+    console.log(this.temp)
+    console.log(this.desc)
    console.log(this.weatherinfo)
   }
 
