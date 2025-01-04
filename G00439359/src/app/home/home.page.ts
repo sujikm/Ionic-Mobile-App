5060a44909e0a1c331d6fef699c809c9949cc9f8
sujikm <kmsujinair@gmail.com>
2025-01-04 15:20:39 +0000

    Added error handling for empty country input in search. Displays a message if no country name is entered.

--- G00439359/src/app/home/home.page.ts
+++ G00439359/src/app/home/home.page.ts
@@ -3,23 +3,30 @@ import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton,IonInput} from '@ionic/angular/standalone';
 import { DataService } from '../services/data.service';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonButton,IonInput,FormsModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonButton,IonInput,FormsModule,CommonModule],
 })
 export class HomePage {
   united:string=""
+  errorMessage: string = '';
   constructor(private router:Router, private ds:DataService) {}
 
   async openCountries(){
+    if (!this.united || this.united.trim() === '') {
+      this.errorMessage = 'Please enter a country name to search.';
+    } else {
+      this.errorMessage = '';
 await this.ds.set("UN",this.united);
 this.router.navigate(['/all-countries'])
+  }
+
   }
   goToSettings() {
     this.router.navigate(['/settings']);
     console.log("image clicked")
   }
-}
+  }
